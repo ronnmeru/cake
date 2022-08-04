9869ecf4c71ef642ca8f741450430f9fca64d3d0
ronnmeru <naoyuki.kojima.0417@gmail.com>
2022-08-04 00:44:36 +0000

    add2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  attachment :image
+  belongs_to :genre
+  has_many :cart_items, dependent: :destroy
+  has_many :order_details
+  with_options presence: true do
+    validates :name
+    validates :introduction
+    validates :image
+    validates :price
+    validates :genre_id
+  end
 end
